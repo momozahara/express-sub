b646fd458529670e9979904751fdf44c15769843
momozahara <momozahara@hotmail.com>
2020-10-25 09:42:05 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const
+    root = (app, domain) => (req, res, callback) => {
+        let hostname = req.headers.host
+
+        if(!domain){
+            let domain = null
+        }
+        if (hostname == 'localhost' || hostname == domain) {
+            return app(req, res, callback)
+        } else {
+            callback()
+        }
+    },
+    sub = (subdomain, app) => (req, res, callback) => {
+        let hostname = req.headers.host
+
+        if(hostname.match(subdomain)) {
+            return app(req, res, callback)
+        } else {
+            callback()
+        }
+    }
+
+module.exports = { root, sub }
